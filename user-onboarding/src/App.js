47d243a88963aeb038a9aea72dc8b94bf1353240
Ian McElroy <mcelroyian@gmail.com>
2020-04-22 18:07:37 -0500

    mvp

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -1,12 +1,122 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
+import * as yup from 'yup'
 import Form from './Form';
 import './App.css';
 
+// Yum Validation schema
+
+const schema = yup.object().shape({
+  name: yup
+    .string(),
+  password: yup
+    .string(),
+  email: yup
+    .string()
+    .email('A valid email address is required')
+    .required('Email is required'),
+  termsAgree: yup
+    .boolean()
+    .oneOf([true], 'Must Accept Terms and Conditions'),
+})
+
+// DATA ENDPOINT
+const url = 'https://reqres.in/api/users'
+
+// USER TEMPLATE
+const userTemplate = {
+  name: '',
+  email: '',
+  password: '',
+  termsAgree: false,
+}
+
+//ERRORS TEMPLATE
+const initialErrors = {
+  email: '',
+  termsAgree: '',
+}
+
 function App() {
+///////////// STATE LIVES HERE
+const [users, setUsers] = useState([])
+const [newUser, SetNewUser] = useState(userTemplate)
+const [isDisabled, setIsDisabled] = useState(true)
+const [errors, setErrors] = useState(initialErrors)
+
+///////////// END OF STATE
+
+///////////// EVENT HANDLES LIVE HERE
+//HANDLE FORM INPUT CHANGE
+const onChange = e => {
+  //save values ahead of time
+  const name = e.target.name
+  const value = e.target.value
+
+  // run validation
+  yup
+    .reach(schema, name)
+    .validate(value)
+    .then(valid => {
+      // clear errors
+      setErrors({
+        ...errors, [name]: '',
+      })
+    })
+    .catch(err => {
+      // add errors
+      setErrors({
+        ...errors, [name]: err.errors[0],
+      })
+    })
+
+  // update state
+  SetNewUser({...newUser, [name]:value})
+}
+
+//HANDLE FORM CHECKBOX CHANGE
+const onCheckChange = e => {
+  const name = e.target.name
+  const value = e.target.checked
+  SetNewUser({...newUser, [name]:value})
+
+}
+
+//HANDLE FORM SUBMIT
+const onSubmit = (e) => {
+  e.preventDefault()
+  sendUser(newUser)
+}
+
+///////////// END OF EVENT HANDLERS
+
+///////////// HELPER FUNCTIONS & EFFECTS
+//SEND DATA TO SERVER
+const sendUser = (userData) => {
+  axios.post(url, userData)
+    .then(res => {
+      let user = res.data
+      setUsers([...users, user])
+    })
+    .catch(err => {
+      debugger
+    })
+}
+
+// VALIDATE FORM
+useEffect(() => {
+  schema.isValid(newUser)
+    .then(isValid => setIsDisabled(!isValid))
+}, [newUser])
+
+///////////// END OFHELPER FUNCTIONS & EFFECTS
+
+// OUTPUT
   return (
     <div className="App">
-      <h2>Hello from App</h2>
-      <Form />
+      <h1>User List</h1>
+      <Form errors={errors} userData={newUser} isDisabled={isDisabled} onChange={onChange} onSubmit={onSubmit} onCheckChange={onCheckChange} />
+      {users.map((user, i) => <pre key={i}>{JSON.stringify(user, null, 2)}</pre>)}
     </div>
   );
 }
--- user-onboarding/src/Form.js
+++ user-onboarding/src/Form.js
@@ -1,9 +1,46 @@
 import React from "react"
 
 function Form(props) {
+
+    const { onSubmit, errors, userData, onChange, onCheckChange, isDisabled } = props
     
     return (
-        <h2>Form</h2>
+        <div className='form-wrapper'>
+        <form>
+            <h2>Add New User</h2>
+                <div className='errors'>
+                {errors.email}
+                {errors.termsAgree}
+                </div>
+                <div className='form'>
+                <label htmlFor='name'>Name:</label>
+                <input 
+                    value={userData.name}
+                    onChange={onChange}
+                    type='text' 
+                    name='name' />
+                <label htmlFor='email'>Email:  </label>
+                <input 
+                    value={userData.email}
+                    onChange={onChange}
+                    type='text' 
+                    name='email' />
+                <label htmlFor='password'>Password:  </label>
+                <input 
+                    value={userData.passord}
+                    onChange={onChange}
+                    type='text' 
+                    name='password' />
+                <label htmlFor='termsAgree'>I agree to the Terms of Service  </label>
+                <input 
+                    type='checkbox'
+                    checked={userData.termsAgree}
+                    onChange={onCheckChange} 
+                    name='termsAgree' />
+                </div>
+                <button onClick={onSubmit} disabled={isDisabled}>Submit</button>
+        </form>
+        </div>
     )
 }
 
